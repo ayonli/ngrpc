20b655f8fd3c153eff2b515819f0bb8d7dc8d45a
A-yon Lee <the@ayon.li>
2023-09-03 04:07:42 +0800

    move cli path for easy installation

--- cli/ngrpc/main.go
+++ cli/ngrpc/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/ayonli/ngrpc/cli/cmd"
+	"github.com/ayonli/ngrpc/cli/ngrpc/cmd"
 	"github.com/ayonli/ngrpc/config"
 	"github.com/ayonli/ngrpc/host"
 )
@@ -12,7 +12,7 @@ import (
 func main() {
 	args := os.Args
 
-	if len(args) > 0 && args[1] == "host-server" {
+	if len(args) > 1 && args[1] == "host-server" {
 		config, err := config.LoadConfig()
 
 		if err != nil {
