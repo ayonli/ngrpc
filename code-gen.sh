2cfa508ca1a461b265d170db762887379a6075a0
A-yon Lee <the@ayon.li>
2023-09-03 03:23:36 +0800

    optimize ignore files

--- code-gen.sh
+++
@@ -1,3 +0,0 @@
-echo "generating code according to the proto files..."
-protoc --proto_path=proto --go_out=./services --go-grpc_out=./services proto/*.proto
-protoc --proto_path=proto --go_out=./services --go-grpc_out=./services proto/github/ayonli/services/*.proto
